e7c5514b8a09ca4160116cede1f065788113f3c1
tyler0921 <a01093173185@gmail.com>
2025-08-20 13:23:20 +0900

    add things

---
+++ README.md
@@ -0,0 +1,6 @@
+### [실습 2-3] Spring Boot와 JPA(Java Persistence API) 활용
+* DTO(Data Transfer Object)
+* Controller
+* Service
+* Repository
+* Entity
--- src/main/java/com/rookies4/MySpringbootLab/controller/BookRestController.java
+++ src/main/java/com/rookies4/MySpringbootLab/controller/BookRestController.java
@@ -3,71 +3,78 @@ package com.rookies4.MySpringbootLab.controller;
 import com.rookies4.MySpringbootLab.entity.Book;
 import com.rookies4.MySpringbootLab.exception.BusinessException;
 import com.rookies4.MySpringbootLab.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
+@RequiredArgsConstructor
 public class BookRestController {
-
     private final BookRepository bookRepository;
 
-    public BookRestController(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
-
-    // 등록
-    @PostMapping
-    public Book createBook(@RequestBody Book book) {
-        return bookRepository.save(book);
-    }
-
-    // 조회
+    // 모든 도서 조회
     @GetMapping
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
     }
 
-    // ID로 특정 조회 (Optional.map / orElse 사용)
+    // ID로 도서 조회
     @GetMapping("/{id}")
     public ResponseEntity<Book> getBookById(@PathVariable Long id) {
-        return bookRepository.findById(id)
-                .map(ResponseEntity::ok)               // 값이 있으면 200 OK
-                .orElse(ResponseEntity.notFound().build()); // 없으면 404
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        //map(Function) T -> R
+        return optionalBook.map(book -> ResponseEntity.ok(book))
+                //.map(ResponseEntity::ok)
+                //.orElse(ResponseEntity.notFound().build()); //Body가 없고, 404 status code만 반환됨
+                .orElseThrow(() -> new BusinessException("Book Not Found", HttpStatus.NOT_FOUND));
     }
 
-    // ISBN으로  조회 (BusinessException 사용)
+    // ISBN으로 도서 조회
     @GetMapping("/isbn/{isbn}")
     public Book getBookByIsbn(@PathVariable String isbn) {
         return bookRepository.findByIsbn(isbn)
-                .orElseThrow(() -> new BusinessException("ISBN " + isbn + "에 해당하는 도서가 없습니다."));
+                .orElseThrow(() -> new BusinessException("Book Not Found", HttpStatus.NOT_FOUND));
     }
 
-    // 정보 수정
-    @PutMapping("/{id}")
-    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book newBook) {
-        return bookRepository.findById(id)
-                .map(book -> {
-                    book.setTitle(newBook.getTitle());
-                    book.setAuthor(newBook.getAuthor());
-                    book.setIsbn(newBook.getIsbn());
-                    book.setPrice(newBook.getPrice());
-                    book.setPublishDate(newBook.getPublishDate());
-                    return ResponseEntity.ok(bookRepository.save(book));
-                })
-                .orElse(ResponseEntity.notFound().build());
+    // 저자명으로 도서 조회
+    @GetMapping("/author/{author}")
+    public List<Book> getBooksByAuthor(@PathVariable String author) {
+        return bookRepository.findByAuthor(author);
     }
 
-    // 삭제
+    // 도서 등록
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        Book savedBook = bookRepository.save(book);
+        return new ResponseEntity<>(savedBook, HttpStatus.CREATED); //CREATED 201
+    }
+
+    // 도서 정보 수정
+    @PatchMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book bookDetail) {
+        Book existBook = bookRepository.findById(id)
+                .orElseThrow(() -> new BusinessException("Book Not Found", HttpStatus.NOT_FOUND));
+        //가격만 변경
+        existBook.setPrice(bookDetail.getPrice());
+
+        Book updatedBook = bookRepository.save(existBook);
+        return ResponseEntity.ok(updatedBook);
+    }
+
+    // 도서 삭제
     @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteBook(@PathVariable Long id) {
-        return bookRepository.findById(id)
-                .map(book -> {
-                    bookRepository.delete(book);
-                    return ResponseEntity.noContent().build(); // 204
-                })
-                .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        //매칭돠는 Book 이 없으면
+        if (!bookRepository.existsById(id)) {
+            return ResponseEntity.notFound().build(); //body는 없고, 404 status code만 반환
+        }
+        bookRepository.deleteById(id);
+        return ResponseEntity.noContent().build(); ////body는 없고, 204 status code만 반환
     }
 }
